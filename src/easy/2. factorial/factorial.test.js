c5691c0d86ea6a3650d012f6794404708dce7906
Yuri Mikushov <mikushov.yu@gmail.com>
2021-03-31 18:17:23 +0300

    chore: fixed test case names

--- src/easy/2. factorial/factorial.test.js
+++ src/easy/2. factorial/factorial.test.js
@@ -1,15 +1,15 @@
 const factorial = require('./factorial')
 
 describe('check factorial()', () => {
-  test('factorial of 0', () => {
+  test('should return factorial of 0', () => {
     expect(factorial(0)).toBe(1)
   })
 
-  test('factorial of 1', () => {
+  test('should return factorial of 1', () => {
     expect(factorial(1)).toBe(1)
   })
 
-  test('factorial of 720', () => {
+  test('should return factorial of 720', () => {
     expect(factorial(6)).toBe(720)
   })
 })
